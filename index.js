0e49bdfcfbf030322d6b4ec238b4ff72123b48fe
Steven <stanli-peter19@hotmail.com>
2021-03-23 21:37:45 +0000

    Bubble Sorting Selected

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-
+//lista de arrays  suits, value, cards
   let suits = ['&spades;', '&clubs;', '&hearts;', '&diams;'];
   let value = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
   let cardsNumbers = [];
-
+// selector asignado
   let drawBtn = document.querySelector("#draw");
   let sortBtn = document.querySelector("#sort");
   let cardLocation = document.querySelector("#cards");
@@ -13,22 +13,22 @@ window.onload = function() {
     cardsNumbers = [];
     let cardsLot = document.querySelector("#input").value;
     cardLocation.innerHTML = " ";
-
+//carga de for para generar las cartas
     for (let i = 0; i < cardsLot; i++) {
       
       let select = document.createElement("div");
       select.innerHTML ="<div class='cardHeader'></div><div class='cardContent'></div><div class='cardFooter'></div>";
       select.classList.add("card");
       cardLocation.appendChild(select);
-
+//random suits, values
       let randomSuits = Math.floor(Math.random() * suits.length );
       let randomValue = Math.floor(Math.random() * value.length);
       cardsNumbers.push(randomValue);
-
+// seleccion de values
       let suitHeader = document.getElementsByClassName("cardHeader");
       let numberContent = document.getElementsByClassName("cardContent");
       let suitFooter = document.getElementsByClassName("cardFooter");
-
+//asignar los suits
       if (randomSuits == 0) {
         let cardHeader = suits[randomSuits];
         let cardFooter = suits[randomSuits];
@@ -65,7 +65,7 @@ window.onload = function() {
   sortBtn.addEventListener("click", function() {
     bubbleSort(cardsNumbers);
   });
-
+//----------------------------------------------------------------------------------------------------
     function bubbleSort(arr) {
     
         for (let wall = arr.length - 1; wall > 0; wall--){
@@ -83,4 +83,5 @@ window.onload = function() {
             }
         }  
     }
+    //----------------------------------------------------------------------------------------------------------
 };
